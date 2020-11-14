6c3fc51c5973e8e82483c7968e20f21cfdff3b8f
jahzlariosa <joseph.lariosa@pantheon.io>
2020-11-14 15:38:39 +0800

    @package Tag

--- archive.php
+++ archive.php
@@ -1,48 +1,69 @@
 <?php
+
 /**
  *
  * @package pahina
  */
+$container = get_theme_mod('pahina_container_type');
 
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<div class="page-wrapper" id="wrapper">
 
-		<?php if ( have_posts() ) : ?>
+	<div class="<?php echo esc_attr($container); ?>">
 
-			<header class="page-header">
+		<div class="row">
+
+			<?php get_template_part('template-part-checker/sidebar', 'left'); ?>
+
+			<main id="primary" class="site-main">
+
+				<?php if (have_posts()) : ?>
+
+					<header class="page-header">
+						<?php
+						the_archive_title('<h1 class="page-title">', '</h1>');
+						the_archive_description('<div class="archive-description">', '</div>');
+						?>
+					</header><!-- .page-header -->
+
 				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+					/* Start the Loop */
+					while (have_posts()) :
+						the_post();
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
+						/*
 				 * Include the Post-Type-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+						get_template_part('template-parts/content', get_post_type());
 
-			endwhile;
+					endwhile;
 
-			the_posts_navigation();
+					the_posts_navigation();
 
-		else :
+				else :
 
-			get_template_part( 'template-parts/content', 'none' );
+					get_template_part('template-parts/content', 'none');
 
-		endif;
-		?>
+				endif;
+				?>
 
-	</main><!-- #main -->
+			</main><!-- #main -->
+
+			<!-- The pagination component -->
+			<?php pahina_pagination(); ?>
+
+			<?php get_template_part('template-part-checker/sidebar', 'right'); ?>
+
+		</div>
+		<!--row-->
+	</div>
+	<!--container-->
+</div>
+<!--wrapper-->
 
 <?php
-get_sidebar();
 get_footer();
